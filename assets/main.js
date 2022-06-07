b7a16f422f108833dc87068759c1dcb67b4aa37c
Kennedy <kennedy.sena87@gmail.com>
2022-06-07 14:31:29 -0300

    commit inicial

---
+++ assets/main.js
@@ -0,0 +1,18 @@
+// DOM Document Object Model
+/* abre e fecha o meni ao clicar no icone: hamburguer e x */
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for(const element of toggle){
+    element.addEventListener('click', function(){
+        nav.classList.toggle('show')
+    })
+}
+
+/* quando clicar em um item do menu, fechar todo o menu */
+const links = document.querySelectorAll('nav ul li a')
+for(const link of links){
+    link.addEventListener('click', function() {
+        nav.classList.remove('show')
+    })
+}
